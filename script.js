3ca0be404872f8491708c29dfadaa337c5119440
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-07-10 13:34:44 +0200

    correct

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ for (let i=1; i<=5; i++){
 
 for (let i=1; i<=100; i++){
      
-    if (i%3==0 & i%5==0){
+    if (i%3==0 && i%5==0){
     console.log('FizzBuzz')}
     else if (i%3==0){
     console.log('Fizz')}
